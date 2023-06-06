1bc0f01011e347021784d3e38633f8f7c5bbd8f5
Brian Henning <henninb@msn.com>
2023-06-06 05:42:50 -0500

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM haproxytech/haproxy-alpine
+COPY ./proxy.crt /etc/ssl/certs/
+COPY ./proxy.key /etc/ssl/certs/proxy.crt.key
 COPY ./haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
--- cert-install.sh
+++ cert-install.sh
@@ -69,20 +69,7 @@ subjectAltName = @alt_names
 
 [alt_names]
 DNS.1 = ${server_name}
-DNS.2 = pfsense.lan
-DNS.3 = finance.lan
-DNS.4 = proxmox.lan
-DNS.5 = ddwrt.lan
-DNS.6 = pihole.lan
-DNS.7 = freshbooks.lan
-DNS.8 = pfsense
-DNS.9 = finance
-DNS.10 = proxmox
-DNS.11 = ddwrt
-DNS.12 = pihole
-DNS.13 = freshbooks
-DNS.14 = zoominfo
-DNS.15 = zoominfo.lan
+DNS.2 = localhost
 EOF
 
 echo Generate an rsa key
